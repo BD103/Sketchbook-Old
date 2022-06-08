880f1ca3d6e93e153460c5d8a00b47508806f5e9
BD103 <59022059+BD103@users.noreply.github.com>
2022-06-07 20:30:53 -0400

    feat: add piston

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,30 @@
+use glutin_window::GlutinWindow as Window;
+use opengl_graphics::{GlGraphics, OpenGL};
+
+use piston::event_loop::{EventSettings, Events};
+use piston::input::{RenderArgs, RenderEvent, UpdateArgs, UpdateEvent};
+use piston::window::WindowSettings;
+
+const OPENGL_VERSION: OpenGL = OpenGL::V3_2;
+const SCREEN_SIZE: [u32; 2] = [1000, 800];
+
 fn main() {
-    println!("Back to hello world");
+    let mut window: Window = WindowSettings::new("Sketchbook", SCREEN_SIZE)
+        .graphics_api(OPENGL_VERSION)
+        .exit_on_esc(true)
+        .build()
+        .unwrap();
+
+    let mut events = Events::new(EventSettings::new());
+
+    // Runloop
+    while let Some(event) = events.next(&mut window) {
+        if let Some(args) = event.render_args() {
+            // Render app
+        }
+
+        if let Some(args) = event.update_args() {
+            // Update app
+        }
+    }
 }
